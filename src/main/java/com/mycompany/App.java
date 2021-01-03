abdd50555af79e4ac1f367b2c33045a193f04d41
root <root@ip-172-31-17-148.us-east-2.compute.internal>
2021-01-03 10:33:56 +0000

    added new files

---
+++ src/main/java/com/mycompany/App.java
@@ -0,0 +1,22 @@
+package com.mycompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+
+
+
+
+
+    }
+}
